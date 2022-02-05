dfeaead0df3edecf4e50a42df9ee59ea79ea037e
ceaguirrep <carlosaguirre5709@gmail.com>
2022-02-04 22:04:53 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-bs9gz7
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-bs9gz7)
---
+++ index.js
@@ -0,0 +1,6 @@
+var municipios_dane = [{"codigo_departamento":"05","codigo_ciudad":"05001000","nombre_departamento":"ANTIOQUIA","nombre_ciudad":"MEDELLÍN"},{"codigo_departamento":"05","codigo_ciudad":"05002000","nombre_departamento":"ANTIOQUIA","nombre_ciudad":"ABEJORRAL"},{"codigo_departamento":"05","codigo_ciudad":"05004000","nombre_departamento":"ANTIOQUIA","nombre_ciudad":"ABRIAQUÍ"},{"codigo_departamento":"05","codigo_ciudad":"05021000","nombre_departamento":"ANTIOQUIA","nombre_ciudad":"ALEJANDRÍA"},{"codigo_departamento":"05","codigo_ciudad":"05030000","nombre_departamento":"ANTIOQUIA","nombre_ciudad":"AMAGÁ"},{"codigo_departamento":"05","codigo_ciudad":"05031000","nombre_departamento":"ANTIOQUIA","nombre_ciudad":"AMALFI"}];
+
+
+var ciudad = 'SABANALARGA'
+
+console.log(municipios_dane[1].nombre_ciudad.indexOf('ABEJORRAL'))
